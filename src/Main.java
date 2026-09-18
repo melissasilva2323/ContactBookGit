5a992351896afa37e198788718460fe17df7b3d6
TiagoAfonsoFCT <ts.afonso@fct.unl.pt>
2026-09-18 12:33:06 +0100

    Changed PHONE_NOT_EXIST constant to match expected output.

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
     public static final String NAME_NOT_EXIST = "contactBook.Contact does not exist.";
-    public static final String PHONE_NOT_EXIST = "contactBook.Phone number does not exist.";
+    public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
     public static final String CONTACT_ADDED = "contactBook.Contact added.";
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
